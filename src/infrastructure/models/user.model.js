8078039bfe210dbab612f5df08a9d8a6a26882cc
Rudra <rudradesai@munoai.com>
2026-04-15 12:20:42 +0530

    feat: enhance model schemas with userId and improve balance validation

--- src/infrastructure/models/user.model.js
+++ src/infrastructure/models/user.model.js
@@ -1,8 +1,20 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema (
+const userSchema = new Schema(
     {
-        
-    }, {timestamps: true});
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            unique: true,
+        },
+        balance: {
+            type: Number,
+            default: 50000,
+            min: [0, "Balance cannot be negative"],
+        },
+    },
+    { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
